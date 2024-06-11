03c76451802f16f60fe445034747d17d4b6a852d
Harley Statum <eiraketh1@gmail.com>
2024-06-11 16:28:09 -0400

    completed tasks 1-3

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Form from './Form'
 
@@ -6,9 +7,17 @@ function App() {
   return (
     <div id="app">
       <nav>
-        {/* NavLinks here */}
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+        <NavLink to="/order">
+          Order
+        </NavLink>
       </nav>
-      {/* Route and Routes here */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/order' element={<Form />} />
+      </Routes>
       <Home />
       <Form />
     </div>
